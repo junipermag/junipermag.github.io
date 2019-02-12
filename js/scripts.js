c6220c78dcadf0d3ce0f1f66c2674b36e7e871a3
Haydon Curteis-Lateo <haydon1900@gmail.com>
2019-02-12 20:18:58 +0000

    WHEN DO YOU GROW OUT OF STUDENT LIFE?

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
 $(document).ready( function() {
-  // Menu Start
+    // Menu Start
+  mobeileMenu();
+    // Menu End
+
+    // Shop Image FadeIn Start
+    if (window.location.pathname == '/shop.html') {
+      shopImage();
+    }
+    // Shop Image FadeIn End
+
+    // Footer Copyright Date Start
+    footerCopyright();
+    // Footer Copyright Date End
+});
+
+
+
+//functions
+
+function mobeileMenu() {
   var menu = $("#menu");
   var menuClose = $("#menuClose");
     menu.click(function(e){
@@ -17,37 +36,42 @@ $(document).ready( function() {
       menuClose.css({display: "none"});
   });
 
-    // Menu End
-
-    // Shop Image FadeIn Start
-    var scroll;
-    var scrollBottom;
-    var didScroll;
-
-    $(window).scroll(function(){
-      scroll = $(window).scrollTop();
-      scrollBottom = scroll + $(window).height();
-      didScroll = true;
-    });
-
-    var shopMagazineImage = [];
-    var shopMagazineImageLoc = [];
-
-for (var i = 0; i < 7; i++) {
-  shopMagazineImage[i] = $("#shopMagazineImageFadeIn"+i);
-  shopMagazineImageLoc[i] = shopMagazineImage[i].offset().top;
 }
 
-    setInterval(function() {
-      if(didScroll){
-        didScroll = false;
-        for (var b = 0; b < 7; b++) {
-          if (scrollBottom >= shopMagazineImageLoc[b]) {
-            shopMagazineImage[b].animate({opacity: 1});
-          }
+function shopImage() {
+  var scroll;
+  var scrollBottom;
+  var didScroll;
+
+  $(window).scroll(function(){
+    scroll = $(window).scrollTop();
+    scrollBottom = scroll + $(window).height();
+    didScroll = true;
+  });
+
+  var shopMagazineImage = [];
+  var shopMagazineImageLoc = [];
+
+  for (var i = 0; i < 7; i++) {
+  shopMagazineImage[i] = $("#shopMagazineImageFadeIn"+i);
+  shopMagazineImageLoc[i] = shopMagazineImage[i].offset().top;
+  }
+
+  setInterval(function() {
+    if(didScroll){
+      didScroll = false;
+      for (var b = 0; b < 7; b++) {
+        if (scrollBottom >= shopMagazineImageLoc[b]) {
+          shopMagazineImage[b].animate({opacity: 1});
         }
       }
-    }, 250);
+    }
+  }, 250);
+}
 
-    // Shop Image FadeIn End
-});
+function footerCopyright() {
+  var copyrightP = $('#copyrightP');
+  var date = new Date().getFullYear();
+
+  copyrightP.html('Juniper &#169; ' + date);
+}
